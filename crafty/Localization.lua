9a009aad0d6e65c280addf202f5111b70bfbd92a
M2us <Dead_Maus@mail.ru>
2017-06-20 21:11:14 +0300

    Added Crafty with support ruRU locale

---
+++ crafty/Localization.lua
@@ -0,0 +1,18 @@
+if (GetLocale() == "ruRU") then
+-- Russian localization by Lichery
+	CHANNEL_NAME = "Введите имя персонажа или канал.";
+	LINK = "Отправить";
+	LINK_1 = "Ссылка";
+	CANCEL = "Отмена";
+	SEARCH = "Поиск";
+	MATS = "Есть мат-лы";
+	NO_RESULTS = "Поиск не дал результатов.";
+	else
+	CHANNEL_NAME = "Enter a character name or channel.";
+	LINK = "Link";
+	LINK_1 = "Link";
+	CANCEL = "Cancel";
+	SEARCH = "Search";
+	MATS = "Mats";
+	NO_RESULTS = "No results matched your search.";
+end
