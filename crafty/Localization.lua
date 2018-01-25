b666bf2fdbcde004a335a4f7b03c84988d2683e4
Артур <Artur91425@gmail.com>
2018-01-26 01:44:23 +0300

    Updated to current author version

--- crafty/Localization.lua
+++ crafty/Localization.lua
@@ -1,18 +1,18 @@
-if (GetLocale() == "ruRU") then
--- Russian localization by Lichery
-	CHANNEL_NAME = "Введите имя персонажа или канал.";
-	LINK = "Отправить";
-	LINK_1 = "Ссылка";
-	CANCEL = "Отмена";
-	SEARCH = "Поиск";
-	MATS = "Есть мат-лы";
-	NO_RESULTS = "Поиск не дал результатов.";
-	else
-	CHANNEL_NAME = "Enter a character name or channel.";
-	LINK = "Link";
-	LINK_1 = "Link";
-	CANCEL = "Cancel";
-	SEARCH = "Search";
-	MATS = "Mats";
-	NO_RESULTS = "No results matched your search.";
+if GetLocale() == "ruRU" then
+	-- Russian localization by Lichery
+	CR_CHANNEL_NAME = "Введите имя персонажа или канал.";
+	CR_LINK = "Отправить";
+	CR_LINK_1 = "Ссылка";
+	CR_CANCEL = "Отмена";
+	CR_SEARCH = "Поиск";
+	CR_MATS = "Есть мат-лы";
+	CR_NO_RESULTS = "Поиск не дал результатов.";
+else
+	CR_CHANNEL_NAME = "Enter a character name or channel.";
+	CR_LINK = "Link";
+	CR_LINK_1 = "Link";
+	CR_CANCEL = "Cancel";
+	CR_SEARCH = "Search";
+	CR_MATS = "Mats";
+	CR_NO_RESULTS = "No results matched your search.";
 end
